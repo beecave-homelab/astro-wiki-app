a47d8b86e4c58f31ac4276abc929420c4273a4c6
elvee <lowie@beecave.nl>
2024-11-30 15:06:50 +0100

    feat ✨: Add environment variable types to ImportMetaEnv

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+    readonly JWT_SECRET: string;
+    readonly COOKIE_SECURE: string;
+    readonly COOKIE_SAME_SITE: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
